214b56c51544363ec50de8932738fbc0898c1d9e
Mike Schwab <mike.schwab@gmail.com>
2022-01-05 23:38:25 -0500

    Initial Commit

---
+++ Shared/AlphabetiX_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphabetiX_AppApp.swift
+//  Shared
+//
+//  Created by Michael Schwab on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AlphabetiX_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
